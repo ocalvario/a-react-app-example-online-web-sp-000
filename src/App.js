2b082ecea248b3fc312609a723ba149554df0288
Oswald Calvario <oswald.calvario@gmail.com>
2020-07-27 04:32:38 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,3 +1,36 @@
+Skip to content
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@ocalvario 
+s-nicas
+/
+a-react-app-example-v-000
+forked from learn-co-students/a-react-app-example-v-000
+0
+0457
+Code
+Pull requests
+Actions
+Projects
+Security
+Insights
+a-react-app-example-v-000/src/App.js /
+@s-nicas
+s-nicas Done.
+Latest commit 1540031 on Oct 30, 2018
+ History
+ 2 contributors
+@s-nicas@DanielSeehausen
+30 lines (25 sloc)  822 Bytes
+  
+Code navigation is available!
+Navigate your code with ease. Click on function and method calls to jump to their definitions or references in the same repository. Learn more
+
 import React, { Component } from 'react';
 import moment from 'moment';
 import ExampleComponent from './ExampleComponent'
@@ -11,14 +44,16 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {moment().format('MMMM Do YYYY, hh:mm:ss a')}
+          Now
         </header>
         <p className="App-intro">
-          In React apps, we write JSX - it looks like HTML, and uses a lot of HTML syntax.
+          In React apps, we write JSX - it looks like HTML, and uses a lot HTML syntax.
           JSX lets us include JavaScript functions right along with the HTML, and also
-          allows us to add in components, which are separate, self-contained chunks of JSX.
+          allows us to add in components, which are separate, self-contained chunks of of JSX.
         </p>
         <ExampleComponent />
+        <ExampleComponent />
+        <TestComponent />
 
       </div>
     );
@@ -26,3 +61,15 @@ class App extends Component {
 }
 
 export default App;
+© 2020 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
